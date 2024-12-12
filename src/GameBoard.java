7e23e61c9a24d78481699a598a130d5395e12b40
Ebram-Wael <202227393@std.sci.cu.edu.eg>
2024-12-12 19:56:15 +0200

    add class gameBoard

---
+++ src/GameBoard.java
@@ -0,0 +1,10 @@
+import java.awt.geom.Point2D;
+import java.util.*;
+public class GameBoard {
+    private ArrayList<Rabbit> rabbits;
+    private String backGround;
+    private int score;
+    private int timeRemaining;
+    private int health;
+
+}
